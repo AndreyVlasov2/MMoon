5acf7688dfa8ed11670cc2e9f7f145e28425b64f
Andrey <andrarduin@mail.ru>
2021-05-31 21:12:17 +0300

    first commit

---
+++ src/main/java/com/mmoon/mmoon/MMoon.java
@@ -0,0 +1,17 @@
+package com.mmoon.mmoon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MMoon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
